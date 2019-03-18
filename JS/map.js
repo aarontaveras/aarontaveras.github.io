4bd9862a3bdd2b6d8e9b0d5a59a4f31ea2a3ab88
Aaron Taveras <aaron@anagram.studio>
2019-03-18 08:55:16 -0700

    Update map.js

--- JS/map.js
+++ JS/map.js
@@ -64,26 +64,35 @@ map.on('style.load', function () {
 // TEST
 var stores = "https://raw.githubusercontent.com/aarontaveras/Sweetgreens/master/sweetgreens.geojson";
 
-map.on('load', () => {
-	fetch(stores)
-		.then(response => response.json())
-		.then((data) => {
-			map.addSource("locations", {
-				type: 'geojson',
-				data: stores
-			});
+map.on('load', function () {
+	map.addSource("list", {
+		type: 'geojson',
+		data: stores
+	});
+	map.addLayer({
+		"id": "locations",
+		"type": "symbol",
+		"source": "list",
+		"layout": {
+			'icon-image': 'circle-15',
+			'icon-allow-overlap': true,
+		}
+	});
+});
 
-			map.addLayer({
-				"id": "locations",
-				"type": "symbol",
-				"source": "locations",
-				"layout": {
-					'icon-image': 'circle-15',
-					'icon-allow-overlap': true,
-				}
-			});
-    buildLocationList(data);
-		});
+map.on('load', () => {
+  fetch(stores)
+    .then(response => response.json())
+    .then((data) => {
+      map.addSource("locations", {
+        type: 'geojson',
+        data: data
+      });
+
+      map.addLayer(["locations"]);
+
+      buildLocationList(data);
+    });
 });
 
 // Load and add list GeoJSON layers
